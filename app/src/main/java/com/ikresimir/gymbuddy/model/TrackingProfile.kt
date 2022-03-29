3925870aeacbac520fa471c7497360a19b6f5ae6
kresohr <48351282+kresohr@users.noreply.github.com>
2022-03-29 10:20:14 +0200

    -Formatting

--- app/src/main/java/com/ikresimir/gymbuddy/model/TrackingProfile.kt
+++ app/src/main/java/com/ikresimir/gymbuddy/model/TrackingProfile.kt
@@ -2,5 +2,8 @@ package com.ikresimir.gymbuddy.model
 
 import java.time.LocalDate
 
-data class TrackingProfile(var weight: Double, var calories: Int, var date: String, var userId: Int) {
+data class TrackingProfile(var weight: Double,
+                           var calories: Int,
+                           var date: String,
+                           var userId: Int) {
 }
